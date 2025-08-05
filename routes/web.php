e8bada92f902d061a560108200d35236e7eef6f3
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-08-05 13:20:25 +0900

    リスト 2-5'

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{msg}', function ($msg) {
+Route::get('hello/{msg?}', function ($msg='no message.') {
     $html = <<<EOF
     <html>
       <head>
