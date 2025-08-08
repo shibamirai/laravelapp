a2cdf215853cb4ebd82fa42a065e03bb2bd8afa9
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-08-08 16:59:56 +0900

    リスト 5-17,18

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,6 +1,11 @@
 <div style="border: solid 1px gray; padding: 0 10px; margin: 10px 0;">
   <p style="color: red;">Message component</p>
   <p style="font-size: 12pt;">{{ $msg }}</p>
+  <hr/>
+  <div style="margin: 0 100px; font-size: 12pt; color: blue;">
+    {{ $slot }}
+  </div>
+  <hr/>
   <ul>
     <li>[id] {{ $data['id'] }}</li>
     <li>[title] {{ $data['title'] }}</li>
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,7 +9,9 @@
 
 @section('content')
   <h2>コンポーネント</h2>
-  <x-package-message id="{{ $id }}" />
+  <x-package-message id="{{ $id }}">
+    <p>※これはコンポーネント内に追加したコンテンツです。</p>
+  </x-package-message>
   <p>※上がコンポーネントの表示です。</p>
 @endsection
 
