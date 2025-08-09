f197c4260f16665dd3a454b45924d810b478aae9
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-08-09 11:55:23 +0900

    リスト 5-30

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,26 +9,35 @@
 
 @section('content')
   <p>{{ $msg }}</p>
+  @if (count($errors) > 0)
+    <div>
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
   <form action="/hello" method="post">
     @csrf
     <div>
       <label style="display: inline-block; width: 75px;" for="name">
         name:
       </label>
-      <input type="text" name="name">
+      <input style="display: inline-block" type="text" name="name" value="{{ old('name') }}">
     </div>
     <div>
       <label style="display: inline-block; width: 75px;" for="mail">
         mail:
       </label>
-      <input type="text" name="mail">
+      <input style="display: inline-block" type="text" name="mail" value="{{ old('name') }}">
     </div>
     <div>
       <label style="display: inline-block; width: 75px;" for="age">
         age:
       </label>
-      <input type="number" name="age">
-      <input type="submit" value="send">
+      <input style="display: inline-block" type="number" name="age" value="{{ old('name') }}">
+      <button type="submit">send</button>
     </div>
   </form>
 @endsection
