096c4e64930e44ec8d8c9c8596f6b5f6e12959d7
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-08-18 14:01:51 +0900

    リスト 6-25

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,7 +10,9 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
     public function index(Request $request) {
-        $items = DB::table('people')->get();
+        $items = DB::table('people')
+            ->orderBy('age', 'asc')
+            ->get();
         return view('hello.index', ['items' => $items]);
     }
 
