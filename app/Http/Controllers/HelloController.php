8beb139c330bf7213c38d3ed5eb443a5134a4653
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-08-18 13:57:49 +0900

    リスト 6-23

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -64,9 +64,10 @@ class HelloController extends Controller
     }
 
     public function show(Request $request) {
-        $id = $request->id;
+        $name = $request->name;
         $items = DB::table('people')
-            ->where('id', '<=', $id)
+            ->where('name', 'like', '%' . $name . '%')
+            ->orWhere('mail', 'like', '%' . $name . '%')
             ->get();
         return view('hello.show', ['items' => $items]);
     }
