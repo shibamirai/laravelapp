d91676d12e81df4bd443cbce8b247e6d8ec9f604
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-08-19 11:57:49 +0900

    リスト 7-15

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('age', function (Builder $builder) {
+            $builder->where('age', '>', 20);
+        });
+    }
+
     public function getData()
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
