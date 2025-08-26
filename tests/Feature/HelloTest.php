dfef7104e2231b264d55de0123f6d23cf65d2c10
shibamirai <t_shiba@miraino-katachi.co.jp>
2025-08-26 10:30:27 +0900

    リスト 8-35

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -1,16 +1,7 @@
 <?php
 
-test('access to "/hello"', function () {
-    $response = $this->get('/hello');
-    $response->assertStatus(200);
-});
-
-test('access to "/person"', function () {
-    $response = $this->get('/person');
-    $response->assertStatus(200);
-});
-
-test('access to "/board"', function () {
-    $response = $this->get('/board');
-    $response->assertStatus(200);
+test('check database User create', function () {
+    // ユーザーを作成
+    $user = App\Models\User::factory()->create();
+    expect($user)->not->toBeNull();
 });
